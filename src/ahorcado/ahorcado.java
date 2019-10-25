374c4ad48559fecb95b0628041eaeb7f4b9f8a52
alumno <alumno@aula1-10>
2019-10-25 13:42:27 +0200

    ahorcado

---
+++ src/ahorcado/ahorcado.java
@@ -0,0 +1,34 @@
+package ahorcado;
+
+import java.util.Scanner;
+
+public class ahorcado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leer;
+		leer= new Scanner(System.in);
+		int fallos = 0;
+		String palabra = "nobita";
+		String letra = "";
+		String vpalabra[]= new String [12];
+		
+		
+		for (int i=0;i<palabra.length();i++) {
+			System.out.println(palabra.charAt(i)); 
+				
+			
+				
+			}
+		}
+		
+		
+		
+	}
+
+	
+	
+	
+	
+
+
